98868d30254ed6511373b37d47067a7d9e5f061c
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-08-30 15:10:17 +0530

    Add ChatWindow component - Display dummy hardcoded messages - Message display style is different between agent and user

---
+++ src/lib/ChatWindow.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	interface Message {
+		text: string;
+		sender: string;
+	}
+	let messages: Message[] = [
+		{
+			text: 'hi',
+			sender: 'user'
+		},
+		{
+			text: 'hello',
+			sender: 'agent'
+		}
+	];
+	let newMessage = '';
+</script>
+
+<div class="border border-gray-300 rounded p-4 w-72 m-5">
+	<div class="flex items-center mb-2">
+		<h4 class="text-lg font-semibold">Chat with an agent</h4>
+	</div>
+
+	<div class="max-h-48 overflow-y-auto mb-2">
+		{#each messages as message (message.text)}
+			<div
+				class="p-2 my-1 rounded max-w-4/5 {message.sender === 'agent'
+					? 'text-right bg-blue-100'
+					: 'text-left bg-green-100'}"
+			>
+				{message.text}
+			</div>
+		{/each}
+	</div>
+
+	<div class="flex items-center">
+		<input
+			type="text"
+			placeholder="Type your message..."
+			class="flex-grow p-2 border rounded-l border-gray-300"
+			bind:value={newMessage}
+		/>
+		<button
+			class="bg-blue-500 text-white border border-blue-500 rounded-r p-2 ml-1
+        hover:bg-blue-600 hover:border-blue-600"
+		>
+			Send
+		</button>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import ChatWindow from '$lib/ChatWindow.svelte';
+</script>
+
+<div class="flex max-w-10vw flex-col p-5">
+	<ChatWindow />
+</div>
