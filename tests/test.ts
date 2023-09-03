6642445c7452d032d2eccecd99a9f3cf05535e54
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-09-03 16:45:09 +0530

    Update README

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,49 @@
-import { expect, test } from '@playwright/test';
+import { chromium } from '@playwright/test';
+import type { Browser, Page } from 'playwright';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+import { getByText, fireEvent, render } from '@testing-library/svelte';
+import App from '../src/App.svelte'; // Adjust the path to your Svelte component
+
+let browser: Browser;
+let page: Page;
+
+beforeAll(async () => {
+	browser = await chromium.launch();
+});
+
+afterAll(async () => {
+	await browser.close();
+});
+
+beforeEach(async () => {
+	page = await browser.newPage();
+	await page.goto('http://localhost:5000'); // Replace with your Svelte app's development server URL
+});
+
+afterEach(async () => {
+	await page.close();
+});
+
+describe('ChatBubble Component E2E Test', () => {
+	it('should open chat bubble and send a message', async () => {
+		const { getByTestId } = render(App); // Render your Svelte component
+		const openChatButton = getByTestId('open-chat-button');
+		const chatInput = getByTestId('chat-input');
+		const sendButton = getByTestId('send-button');
+
+		// Open chat bubble
+		await fireEvent.click(openChatButton);
+		await page.waitForSelector('div[data-test="chat-bubble"]');
+
+		// Send a message
+		await fireEvent.input(chatInput, { target: { value: 'Hello, this is a test message.' } });
+		await fireEvent.click(sendButton);
+
+		// Wait for the message to appear
+		await page.waitForSelector(
+			'div[data-test="chat-message"]:has-text("Hello, this is a test message.")'
+		);
+	});
+
+	// Add more test cases for other Svelte components as needed
 });
